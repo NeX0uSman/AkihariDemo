cd04e9ebf33ee9bb03d8210cef3740192f8d4151
Artem <artemkirpotenko@gmail.com>
2026-04-27 23:59:03 +0200

    FAQ animation

--- src/components/FaqParentBlock.tsx
+++ src/components/FaqParentBlock.tsx
@@ -12,18 +12,43 @@ const FaqParentBlock = ({ categoryObject }: Props) => {
   const [activeQuestion, setActiveQuestion] = useState<string | null>(null);
 
   return (
-    <div className="rounded-xl flex flex-col gap-4 xl:p-4">
-      <p className="text-3xl text-(--text-muted) pb-2 border-b border-(--border-subtle) font-bold">{categoryObject.categoryTitle}</p>
+    <div
+      style={{
+        animationDelay: `0.1s`,
+        animationFillMode: "both",
+      }}
+      className="rounded-xl flex flex-col gap-4 xl:p-4"
+    >
+      <p
+        style={{
+          animationDelay: `0.1s`,
+          animationFillMode: "both",
+        }}
+        className="text-3xl text-(--text-muted) pb-2 border-b border-(--border-subtle) font-bold animate-fadeUp"
+      >
+        {categoryObject.categoryTitle}
+      </p>
       <div className="flex flex-col gap-2">
-        {categoryObject.questions.map((question: Question) => (
-          <FaqChildBlock
+        {categoryObject.questions.map((question: Question, index) => (
+          <div
             key={question.q}
-            question={question}
-            isActive={activeQuestion === question.q}
-            onToggle={() => setActiveQuestion(
-              activeQuestion === question.q ? null : question.q
-            )}
-          />
+            style={{
+              animationDelay: `${(index + 1) * 0.1}s`,
+              animationFillMode: "both",
+            }}
+            className="animate-fadeUp"
+          >
+            <FaqChildBlock
+              key={question.q}
+              question={question}
+              isActive={activeQuestion === question.q}
+              onToggle={() =>
+                setActiveQuestion(
+                  activeQuestion === question.q ? null : question.q,
+                )
+              }
+            />
+          </div>
         ))}
       </div>
     </div>
